6f17ee54850f9fcfe7372ecf821e4f7c3ae8aab7
Magister <xqjh@mail.ru>
2023-10-29 09:19:59 +0300

    reset strings for WorkPlace fragment

--- app/src/main/java/ru/practicum/android/diploma/WorkPlace.kt
+++ app/src/main/java/ru/practicum/android/diploma/WorkPlace.kt
@@ -5,17 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.databinding.FragmentWorkPlaceBinding
 
 class WorkPlace : Fragment() {
     private var _binding: FragmentWorkPlaceBinding? = null
     private val binding:FragmentWorkPlaceBinding get() = _binding!!
+
+    private fun setUiListeners(){
+        with(binding){
+            navigationBar.setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentWorkPlaceBinding.inflate(inflater,container,false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_work_place, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUiListeners()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
