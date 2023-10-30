2b360e4ca77a2e5c23825e567d52eb61eeba9784
Magister <xqjh@mail.ru>
2023-10-30 17:43:30 +0300

    Добавлен фрагмент Вакансии. Для фрагмента установлен один параметр vacancy (временно String) для передачи модели вакансии на отображение. Реализован переход с избранного на детализацию вакансии

--- app/src/main/java/ru/practicum/android/diploma/Favourite.kt
+++ app/src/main/java/ru/practicum/android/diploma/Favourite.kt
@@ -5,11 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.databinding.FragmentFavouriteBinding
 
 class Favourite : Fragment() {
     private  var _binding: FragmentFavouriteBinding? = null
     private val binding get() = _binding!!
+
+    private fun setUiListeners(){
+        binding.txtTemporal.setOnClickListener {
+            openFragmentVacancy(vacancyToShow = "Вакансия из избранного")
+        }
+    }
+
+    /**
+     * Function to open required vacancy details
+     * @param vacancyToShow represent vacancy model
+     * @author Oleg
+     */
+    // TODO: change param type
+    private fun openFragmentVacancy(vacancyToShow:String){
+        findNavController().navigate(R.id.action_favourite_to_vacancy,Bundle().apply { putString("vacancy_model",vacancyToShow) })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +37,13 @@ class Favourite : Fragment() {
         // Inflate the layout for this fragment
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUiListeners()
+    }
+
+
 
     override fun onDestroy() {
         super.onDestroy()
