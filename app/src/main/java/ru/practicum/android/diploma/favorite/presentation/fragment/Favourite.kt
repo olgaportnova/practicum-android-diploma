d8f7a5f2421ee91c53131434497a59796b0ee798
OlegWork <xqjh@mail.ru>
2023-10-31 13:43:29 +0300

    Fix merge troubles again

--- app/src/main/java/ru/practicum/android/diploma/favorite/presentation/fragment/Favourite.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorite/presentation/fragment/Favourite.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFavouriteBinding
 
 class Favourite : Fragment() {
--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/fragment/District.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/fragment/District.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentDistrictBinding
 const val ARG_COUNTRY_ID = "country_id_pram"
 
