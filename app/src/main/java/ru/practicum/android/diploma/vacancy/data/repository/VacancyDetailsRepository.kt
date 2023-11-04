44ef80d1d07629068399ea4c579054bdef3386c4
TanyaRul <t.rul26@yandex.ru>
2023-11-04 15:06:49 +0300

    - removes models from the vacancy package, replaces them with models from the search package - fixes the return result of the interactor - removes serialization from search results

--- app/src/main/java/ru/practicum/android/diploma/vacancy/data/repository/VacancyDetailsRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/data/repository/VacancyDetailsRepository.kt
@@ -1,9 +1,9 @@
 package ru.practicum.android.diploma.vacancy.data.repository
 
 import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.search.domain.models.Vacancy
 import ru.practicum.android.diploma.util.DataResource
-import ru.practicum.android.diploma.vacancy.domain.models.VacancyDetails
 
 interface VacancyDetailsRepository {
-    suspend fun getVacancyDetails(id: Int): Flow<DataResource<VacancyDetails>>
+    suspend fun getVacancyDetails(id: Int): Flow<DataResource<Vacancy>>
 }
--- app/src/main/java/ru/practicum/android/diploma/vacancy/domain/models/SearchResult.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/domain/models/SearchResult.kt
@@ -1,8 +1,6 @@
 package ru.practicum.android.diploma.vacancy.domain.models
 
-import java.io.Serializable
-
 data class SearchResult<out A, out B>(
-    val resultList: A,
+    val result: A,
     val error: B
-) : Serializable
+)
