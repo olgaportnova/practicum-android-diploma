cb0f70c43b6da5599d4b5947c37dde8a4b6411a4
Magister <xqjh@mail.ru>
2023-11-04 15:14:50 +0300

    Reformat fragment_work_place design Add special behaviour for component in cases of selection or deselection country or district

--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/fragment/WorkPlace.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/fragment/WorkPlace.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -28,11 +29,11 @@ class WorkPlace : DefaultFragment<FragmentWorkPlaceBinding>() {
                 exitExtraWhenSystemBackPushed()
             }
 
-            btnChooseCountry.setOnClickListener {
+            txtChooseCountry.setOnClickListener {
                 findNavController().navigate(R.id.action_to_country)
             }
 
-            btnChooseDistrict.setOnClickListener {
+            txtChooseDistrict.setOnClickListener {
                 val parentId = when (vm.countryChosen.value) {
                     null -> 113
                     else -> vm.countryChosen.value!!.id
@@ -41,6 +42,16 @@ class WorkPlace : DefaultFragment<FragmentWorkPlaceBinding>() {
                 findNavController().navigate(
                     R.id.action_to_district,
                     Bundle().apply { putInt(ARG_COUNTRY_ID, parentId) })
+            }
+
+            btnClrCountry.setOnClickListener {
+                // Функция вызванная с нулевыми параметрами обнуляет поле _countryChosen in viewModel
+                vm.chooseAnotherCountry(null,null)
+            }
+
+            btnClrDistrict.setOnClickListener {
+                // Функция вызванная с нулевыми параметрами обнуляет поле _districtChosen in viewModel
+                vm.chooseAnotherDistrict(null,null)
             }
         }
     }
@@ -77,11 +88,45 @@ class WorkPlace : DefaultFragment<FragmentWorkPlaceBinding>() {
         super.onViewCreated(view, savedInstanceState)
 
         vm.countryChosen.observe(viewLifecycleOwner) {
-            binding.btnChooseCountry.text = it.name
+            binding.lblChooseCountry.isVisible = it!=null
+
+            if(it!=null) {
+                // Если выбрана страна, устанавливаем заголовок и ставим иконку "стереть"
+                binding.txtChooseCountry.text = it.name
+                binding.btnClrCountry.setImageResource(R.drawable.ic_clear)
+
+                // Так же устанавливаем цвет текста
+                binding.txtChooseCountry.setTextColor(resources.getColor(R.color.blue_main,requireContext().theme))
+            }
+            else{
+                binding.txtChooseCountry.text = getString(R.string.work_place_country_title)
+                binding.btnClrCountry.setImageResource(R.drawable.baseline_arrow_forward_24)
+
+                // Так же устанавливаем цвет текста
+                binding.txtChooseCountry.setTextColor(resources.getColor(R.color.grey_dark,requireContext().theme))
+            }
         }
 
         vm.districtChosen.observe(viewLifecycleOwner) {
-            binding.btnChooseDistrict.text = it.name
+            binding.lblChooseDistrict.isVisible = it!=null
+
+            if(it!=null) {
+                // Если выбран район, устанавливаем заголовок и ставим иконку "стереть"
+                binding.txtChooseDistrict.text = it.name
+                binding.btnClrDistrict.setImageResource(R.drawable.ic_clear)
+
+                // Так же устанавливаем цвет текста
+                binding.txtChooseDistrict.setTextColor(resources.getColor(R.color.blue_main,requireContext().theme))
+            }
+            else{
+                // Если район не выбран или стерт удаляем заголовок и ставим иконку "forward"
+                binding.txtChooseDistrict.text = getString(R.string.work_place_district_title)
+                binding.btnClrDistrict.setImageResource(R.drawable.baseline_arrow_forward_24)
+
+                // Так же устанавливаем цвет текста
+                binding.txtChooseDistrict.setTextColor(resources.getColor(R.color.grey_dark,requireContext().theme))
+            }
+
         }
 
         vm.errorMsg.observe(viewLifecycleOwner) {
--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/view_model/WorkPlaceVm.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/view_model/WorkPlaceVm.kt
@@ -1,55 +1,29 @@
 package ru.practicum.android.diploma.filter.presentation.view_model
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
-import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.filter.domain.models.Area
-import ru.practicum.android.diploma.hhApi.impl.RetrofitClient
 
 class WorkPlaceVm : ViewModel() {
-    private val _countryChosen = MutableLiveData<Area>()
-    val countryChosen = _countryChosen as LiveData<Area>
+    private val _countryChosen = MutableLiveData<Area?>(null)
+    val countryChosen = _countryChosen as LiveData<Area?>
 
-    private val _districtChosen = MutableLiveData<Area>()
-    val districtChosen = _districtChosen as LiveData<Area>
+    private val _districtChosen = MutableLiveData<Area?>(null)
+    val districtChosen = _districtChosen as LiveData<Area?>
 
     private val _errorMsg = MutableLiveData<String>()
     val errorMsg = _errorMsg as LiveData<String>
 
-    init {
-        val token = BuildConfig.HH_ACCESS_TOKEN
-        Log.e("LOG",token)
-
-        val retro = RetrofitClient()
-        viewModelScope.launch {
-            val response = retro.loadVacanciesByArea()
-            when(response.code()){
-                200-> {
-                    Log.e("LOG", "200")
-                    response.body()?.let {
-                        Log.e("LOG", "found ${it.found} ")
-                    }
-                }
-                else->{
-                    Log.e("LOG","${response.code()}")
-                }
-            }
-        }
+    fun chooseAnotherCountry(newCountryId: Int?, newCountryName: String?) {
+        if (newCountryId != null && newCountryName != null) {
+            _countryChosen.value = Area(newCountryId, null, newCountryName, emptyList())
+        } else _countryChosen.value = null
     }
 
-    fun chooseAnotherCountry(newCountryId: Int?,newCountryName:String?) {
-        if(newCountryId!=null&&newCountryName!=null){
-            _countryChosen.value = Area(newCountryId,null,newCountryName, emptyList())
-        }
-    }
-
-    fun chooseAnotherDistrict(newCountryId: Int?,newCountryName:String?) {
-        if(newCountryId!=null&&newCountryName!=null){
-            _districtChosen.value = Area(newCountryId,null,newCountryName, emptyList())
-        }
+    fun chooseAnotherDistrict(newCountryId: Int?, newCountryName: String?) {
+        if (newCountryId != null && newCountryName != null) {
+            _districtChosen.value = Area(newCountryId, null, newCountryName, emptyList())
+        } else _districtChosen.value = null
     }
 }
