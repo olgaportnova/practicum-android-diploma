b85a861e0f8b6a838f3a99ffc5bcba2ddf3a6964
x-mike <hr.misha@yandex.ru>
2023-11-05 14:29:59 +0300

    Changed func DataStatus end move in util

---
+++ app/src/main/java/ru/practicum/android/diploma/util/DataStatus.kt
@@ -0,0 +1,11 @@
+package ru.practicum.android.diploma.util
+
+const val DEFAULT_NETWORK_RESPONSE = 0
+
+sealed class DataStatus<out T>(val code: Int = DEFAULT_NETWORK_RESPONSE, val data: T? = null) {
+    class Loading: DataStatus<Nothing>()
+    class Content<T>(networkData:T) : DataStatus<T>(data = networkData)
+    class EmptyContent: DataStatus<Nothing>()
+    class Error(newResponseCode:Int = DEFAULT_NETWORK_RESPONSE,errorMessage:String = "Error") : DataStatus<Nothing>(code = newResponseCode)
+    class NoConnecting(): DataStatus<Nothing>()
+}
