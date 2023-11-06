f20a25566914aaa0ff190b76320034fee0bc8a77
x-mike <hr.misha@yandex.ru>
2023-11-06 11:45:46 +0300

    Create QueryMap func and mapper

--- app/src/main/java/ru/practicum/android/diploma/search/data/impl/SearchRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/impl/SearchRepositoryImpl.kt
@@ -3,23 +3,48 @@ package ru.practicum.android.diploma.search.data.impl
 import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.filter.domain.models.ParamsFilterModel
 import ru.practicum.android.diploma.hhApi.NetworkClient
+import ru.practicum.android.diploma.search.data.dto.models.QuerySearchMdlDto
 import ru.practicum.android.diploma.search.domain.SearchRepositry
 import ru.practicum.android.diploma.search.domain.models.AnswerVacancyList
 import ru.practicum.android.diploma.search.domain.models.QuerySearchMdl
 import ru.practicum.android.diploma.sharedPref.FiltersStorage
+import ru.practicum.android.diploma.util.mappers.QuerySearchDtoMapper
 
 class SearchRepositoryImpl(
     private val networkClient: NetworkClient,
     private val filtersStorage: FiltersStorage
 ) : SearchRepositry {
     override suspend fun doRequestSearch(searchRequest: QuerySearchMdl): Flow<AnswerVacancyList> {
-        TODO("Not yet implemented")
+        val mapped = QuerySearchDtoMapper.qrSearchMdlToQrSearchMdlDto(searchRequest)
+
+        createQueryMap(mapped)
+
+
     }
 
     override fun getParamsFilters(): ParamsFilterModel? {
         TODO("Not yet implemented")
     }
 
-    fun
-    private fun createQueryMap(searchRequest: QuerySearchMdl){}
+    private fun createQueryMap(searchRequest: QuerySearchMdlDto): Map<String,Any>{
+        val options: HashMap<String,Any> = HashMap()
+
+         options["page"] = searchRequest.page
+         options["per_page"] = searchRequest.perPage
+         options["text"] = searchRequest.text
+         if(searchRequest.area != null)
+             options["area_id"] = searchRequest.area
+         if(searchRequest.parentArea != null)
+             options["area_id"] = searchRequest.parentArea
+         if(searchRequest.industry != null)
+             options["industry"] = searchRequest.industry
+         if(searchRequest.currency != null)
+             options["currency"] = searchRequest.currency
+         if(searchRequest.salary != null)
+             options["salary"] = searchRequest.salary
+         if(searchRequest.onlyWithSalary)
+             options["only_with_salary"] = searchRequest.onlyWithSalary
+
+        return options
+    }
 }
--- app/src/main/java/ru/practicum/android/diploma/util/mappers/QuerySearchDtoMapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/mappers/QuerySearchDtoMapper.kt
@@ -1,4 +1,24 @@
 package ru.practicum.android.diploma.util.mappers
 
+import ru.practicum.android.diploma.search.data.dto.models.QuerySearchMdlDto
+import ru.practicum.android.diploma.search.domain.models.QuerySearchMdl
+
 object QuerySearchDtoMapper {
+
+    fun qrSearchMdlToQrSearchMdlDto(model:QuerySearchMdl): QuerySearchMdlDto{
+        return with(model){
+            QuerySearchMdlDto(
+                page = page,
+                perPage = perPage,
+                text = text,
+                area = area,
+                parentArea = parentArea,
+                industry = industry,
+                currency = currency,
+                salary = salary,
+                onlyWithSalary = onlyWithSalary
+
+            )
+        }
+    }
 }
