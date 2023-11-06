6ae58598f83c98e1c52d1dff15b772a8fe8d0693
Magister <xqjh@mail.ru>
2023-11-06 15:40:12 +0300

    Fragment filters became child from DefaultFragment Set Industry ResultListener

--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/fragment/Filters.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/fragment/Filters.kt
@@ -2,22 +2,39 @@ package ru.practicum.android.diploma.filter.presentation.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFiltersBinding
+import ru.practicum.android.diploma.filter.presentation.util.INDUSTRY_ID
+import ru.practicum.android.diploma.filter.presentation.util.INDUSTRY_NAME
+import ru.practicum.android.diploma.filter.presentation.util.KEY_INDUSTRY_RESULT
+import ru.practicum.android.diploma.util.DefaultFragment
 
-class Filters : Fragment() {
-    private var _binding: FragmentFiltersBinding? = null
-    private val binding get() = _binding!!
+class Filters : DefaultFragment<FragmentFiltersBinding>() {
+    override fun bindingInflater(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentFiltersBinding {
+        return FragmentFiltersBinding.inflate(inflater, container, false)
+    }
 
-    private fun setUiListeners() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setFragmentResultListener(KEY_INDUSTRY_RESULT) { requestKey, bundle ->
+            val industryName = bundle.getString(INDUSTRY_NAME)
+            val industryId = bundle.getInt(INDUSTRY_ID)
+
+            showMsgDialog(industryName.toString())
+        }
+    }
+
+    override fun setUiListeners() {
         with(binding) {
             navigationBar.setNavigationOnClickListener {
                 //parentFragmentManager.setFragmentResult("key", Bundle().apply { putString("bundleKey","Возврат с фрагмента Фильтры. Можно выполнить любую функцию") })
-                findNavController().popBackStack()
+                exitExtraWhenSystemBackPushed()
             }
 
             btnChooseCountry.setOnClickListener {
@@ -33,26 +50,7 @@ class Filters : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentFiltersBinding.inflate(inflater, container, false)
-
-
-        // Inflate the layout for this fragment
-        return binding.root
+    override fun exitExtraWhenSystemBackPushed() {
+        findNavController().popBackStack()
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        setUiListeners() // All listeners set
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
-    }
-
 }
--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/fragment/Industry.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/fragment/Industry.kt
@@ -2,7 +2,12 @@ package ru.practicum.android.diploma.filter.presentation.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.filter.presentation.util.INDUSTRY_ID
+import ru.practicum.android.diploma.filter.presentation.util.INDUSTRY_NAME
+import ru.practicum.android.diploma.filter.presentation.util.KEY_INDUSTRY_RESULT
 import ru.practicum.android.diploma.filter.presentation.util.ParentDataFragment
 import ru.practicum.android.diploma.filter.presentation.view_model.IndustryVm
 
@@ -14,5 +19,19 @@ class Industry : ParentDataFragment() {
         binding.navigationBar.title = "Индустрия"
     }
 
+    override fun exitExtraWhenSystemBackPushed() {
+        // Для поиска вакансии по региону необходимо передать в поисковый запрос id региона
+        setFragmentResult(KEY_INDUSTRY_RESULT, getBackSendData())
+        findNavController().popBackStack()
+    }
+
+    override fun getBackSendData(): Bundle {
+        return Bundle().apply {
+            vm.dataToSendBack?.let {
+                putInt(INDUSTRY_ID, it.id)
+                putString(INDUSTRY_NAME, it.name)
+            }
+        }
+    }
 
 }
