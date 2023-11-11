b3d023474176daf7e1f04f8e90357201443b1712
x-mike <hr.misha@yandex.ru>
2023-11-11 13:29:30 +0300

    Deleted some comments

--- app/src/main/java/ru/practicum/android/diploma/search/domain/models/QuerySearchMdl.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/models/QuerySearchMdl.kt
@@ -7,7 +7,6 @@ data class QuerySearchMdl(
     var area:String? = null,
     var parentArea:String? = null,
     var industry:String? = null,
-    //С данным полем есть вопросы. Необходимо ли оно в дальнейшем или нет.
     var currency:String? = null,
     var salary:Int? = null,
     var onlyWithSalary:Boolean = false
