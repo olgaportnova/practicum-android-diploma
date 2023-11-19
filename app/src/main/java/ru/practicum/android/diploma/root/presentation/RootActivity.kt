9854173a9ce07d602aa32dafb3c274ff04e0faa6
Magister <xqjh@mail.ru>
2023-11-19 21:27:00 +0300

    Add navigationAnimation

--- app/src/main/java/ru/practicum/android/diploma/root/presentation/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/presentation/RootActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRootBinding
+
+    private var navigateCurrentLevel = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRootBinding.inflate(layoutInflater)
@@ -21,7 +23,32 @@ class RootActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
-        navView.setupWithNavController(navController)
+
+
+        //navView.setupWithNavController(navController)
+
+
+        binding.rootNavBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.search -> {
+                    navController.navigate(R.id.search, null, calculateAnimation(0,navigateCurrentLevel))
+                    true
+                }
+
+                R.id.favourite -> {
+                    navController.navigate(R.id.favourite, null, calculateAnimation(1,navigateCurrentLevel))
+                    true
+                }
+
+                R.id.crew -> {
+                    navController.navigate(R.id.crew, null, calculateAnimation(2,navigateCurrentLevel))
+                    true
+                }
+
+                else -> false
+            }
+        }
+
 
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
             when (destination.id) {
@@ -30,6 +57,13 @@ class RootActivity : AppCompatActivity() {
                     binding.divider.visibility = View.GONE
                 }
 
+                R.id.crew -> {
+                    navigateCurrentLevel = 3
+                    binding.rootNavBar.menu.findItem(destination.id).isChecked = true
+                }
+                R.id.favourite -> navigateCurrentLevel = 1
+                R.id.search -> navigateCurrentLevel = 0
+
                 else -> {
                     navView.isVisible = true
                     binding.divider.visibility = View.VISIBLE
@@ -37,5 +71,26 @@ class RootActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun calculateAnimation(newDestinationLevel: Int,previousDestinationLevel:Int):NavOptions {
+        if(newDestinationLevel>previousDestinationLevel){
+            // Листаем слайд справа налево (перелистываем)
+            return NavOptions.Builder()
+                .setEnterAnim(R.anim.slide_out_right) // анимация для входа во фрагмент
+                .setExitAnim(R.anim.slide_in_left) // анимация для выхода из фрагмента
+                .setPopEnterAnim(R.anim.slide_out_right) // анимация для входа при возврате назад
+                .setPopExitAnim(R.anim.slide_in_left) // анимация для выхода при возврате назад
+                .build()
+        }
+        else{
+            return NavOptions.Builder()
+                .setEnterAnim(R.anim.slide_out_left) // анимация для входа во фрагмент
+                .setExitAnim(R.anim.slide_in_right) // анимация для выхода из фрагмента
+                .setPopEnterAnim(R.anim.slide_out_left) // анимация для входа при возврате назад
+                .setPopExitAnim(R.anim.slide_in_right) // анимация для выхода при возврате назад
+                .build()
+        }
+    }
+
 
 }
